4b56c6902ba84f9c345b1ff81da2a98b61e5cd5a
ryanwmiecz <ryski03@gmail.com>
2024-04-21 08:32:03 -0400

    main menu

--- Control.gd
+++ Control.gd
@@ -1,14 +1,32 @@
 extends Control
 
+@onready var text = $MarginContainer/HBoxContainer/VBoxContainer/Options/Tuto
+@onready var c2 = $MarginContainer/HBoxContainer/VBoxContainer/Options/ColorRect2
+@onready var c1 = $MarginContainer/HBoxContainer/VBoxContainer/Options/ColorRect
+@onready var c3 = $MarginContainer/HBoxContainer/VBoxContainer/Options/ColorRect3
 
 
+func _ready():
+	text.hide()
+	c1.hide()
+	c2.hide()
+	c3.hide()
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Levels/Board.tscn")
 
 
 func _on_options_pressed():
-	pass # Replace with function body.
+	if (text.visible == true):
+		text.visible = false
+		c1.visible = false
+		c2.visible = false
+		c3.visible = false
+	else:
+		text.visible = true
+		c1.visible = true
+		c2.visible = true
+		c3.visible = true
 
 
 
