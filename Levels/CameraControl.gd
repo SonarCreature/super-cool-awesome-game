af973df8b7fd3ab5459eeab40cbce0274d450115
DogLoaf0 <aaronwcfl@gmail.com>
2024-04-21 07:52:10 -0400

    Camera Controls

---
+++ Levels/CameraControl.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var camspeed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	velocity = input_direction * camspeed
+	
+	move_and_slide()
+	pass
